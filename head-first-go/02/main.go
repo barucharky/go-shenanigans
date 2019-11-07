147aeeb98e3d7260c356c3c946ed0586568d04ce
Baruch Arky <benarky@gmail.com>
2019-11-07 16:16:27 +0200

    worked on 02

--- head-first-go/02/main.go
+++ head-first-go/02/main.go
@@ -5,12 +5,28 @@ import (
 	"fmt"
 	"os"
 	"reflect"
+	"strings"
 )
 
 func main() {
+	// input
 	reader := bufio.NewReader(os.Stdin)
+	question := "how are you\n"
+
+	fmt.Println("type something")
 	input, _ := reader.ReadString('\n')
+
+	if input == question {
+		fmt.Println("\nI am functioning within established parameters.\n")
+	}
 
 	fmt.Println("Reader type:", reflect.TypeOf(reader))
 	fmt.Println("Input type:", reflect.TypeOf(input))
+
+	// strings
+	broken := "G# r#cks!"
+	replacer := strings.NewReplacer("#", "o")
+	fixed := replacer.Replace(broken)
+	fmt.Println("\nfixed string:")
+	fmt.Println(fixed)
 }
