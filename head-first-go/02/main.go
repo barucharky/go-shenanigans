d7d0c2a64084b3827a34094354e92b1298285488
Baruch Arky <benarky@gmail.com>
2019-11-08 12:44:28 +0200

    more work on 02

--- head-first-go/02/main.go
+++ head-first-go/02/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math/rand"
 	"os"
 	"reflect"
 	"strings"
@@ -17,9 +18,11 @@ func main() {
 	input, _ := reader.ReadString('\n')
 
 	if input == question {
-		fmt.Println("\nI am functioning within established parameters.\n")
+		fmt.Println("~------------~")
+		fmt.Println("I am functioning within established parameters.")
 	}
 
+	fmt.Println("~------------~")
 	fmt.Println("Reader type:", reflect.TypeOf(reader))
 	fmt.Println("Input type:", reflect.TypeOf(input))
 
@@ -27,6 +30,27 @@ func main() {
 	broken := "G# r#cks!"
 	replacer := strings.NewReplacer("#", "o")
 	fixed := replacer.Replace(broken)
-	fmt.Println("\nfixed string:")
+	fmt.Println("~------------~")
+	fmt.Println("fixed string:")
 	fmt.Println(fixed)
+
+	// random number
+	// why does this always produce the same number?
+	target := rand.Intn(100) + 1
+	fmt.Println("~------------~")
+	fmt.Println("Random number:")
+	fmt.Println(target)
+
+	// loop
+	fmt.Println("~------------~")
+	for x := 4; x <= 6; x++ {
+		fmt.Println("x is now", x)
+	}
+
+	fmt.Println("~------------~")
+	y := 15
+	for y >= 5 {
+		fmt.Println("y is now", y)
+		y -= 5
+	}
 }
