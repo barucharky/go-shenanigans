42af1d374c7be715065d971e50184daccb2af8b2
barucharky <benarky@gmail.com>
2020-03-04 13:58:00 +0200

    practice with func types

--- head-first-go/11/11g-func-types/sandbox/main.go
+++ head-first-go/11/11g-func-types/sandbox/main.go
@@ -6,12 +6,11 @@ import "fmt"
 
 // -- --------------------------------------
 // You can declare a function signature as a type:
-
 type myFunctionType = func(a string, b string) string
 
 func wazzup(fn myFunctionType) {
 
-	var newStr string = fn("hello", "world")
+	var newStr = fn("hello", "world")
 
 	fmt.Println(newStr)
 }
@@ -21,27 +20,28 @@ func main() {
 
 	// -- ----------------------------------
 	// You can be explicit about what func type your func is going to implement:
+
 	var implementation1 myFunctionType = func(a string, b string) string {
-		return fmt.Sprintf("%s and %s", a, b)
+		return fmt.Sprintf("%s+%s", a, b)
 	}
 
 	wazzup(implementation1)
 
 	// -- ----------------------------------
 	// You can be implicit:
-	implementation2 := func(a, b string) string {
-		return fmt.Sprintf("%s %s", b, a)
+
+	implementation2 := func(a string, b string) string {
+		return fmt.Sprintf("%s to the %s", a, b)
 	}
 
 	wazzup(implementation2)
 
 	// -- ----------------------------------
 	// You can even be anonymous:
-	wazzup(
 
-		func(a, b string) string {
-			return fmt.Sprintf("%s %s!", a, b)
+	wazzup(
+		func(a string, b string) string {
+			return fmt.Sprintf("%s, I want to say %s", b, a)
 		},
 	)
-
 }
