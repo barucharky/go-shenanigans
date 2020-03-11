e5ef344cde3da511d52576cbd8dde0d009ca289a
barucharky <benarky@gmail.com>
2020-03-11 18:52:21 +0200

    practice with function types

--- head-first-go/11/11g-func-types/sandbox/main.go
+++ head-first-go/11/11g-func-types/sandbox/main.go
@@ -4,44 +4,65 @@ package main
 
 import "fmt"
 
-// -- --------------------------------------
-// You can declare a function signature as a type:
+// Declare the function type's signature
 type myFunctionType = func(a string, b string) string
 
+// Write a function to implement the functions
 func wazzup(fn myFunctionType) {
 
-	var newStr = fn("hello", "world")
+	var newStr string = fn("hello", "world")
 
 	fmt.Println(newStr)
 }
 
-// -- --------------------------------------
+// Here's an implementation to use later
+func implementation3(a string, b string) string {
+
+	return "ashemoooooorrgissBBOOOOOOORDddah!!!!"
+}
+
 func main() {
 
-	// -- ----------------------------------
-	// You can be explicit about what func type your func is going to implement:
-
+	// Explicit implementation
 	var implementation1 myFunctionType = func(a string, b string) string {
-		return fmt.Sprintf("%s+%s", a, b)
+
+		return fmt.Sprintf("%s %s", a, b)
 	}
+
+	fmt.Println("Implementation 1:")
 
 	wazzup(implementation1)
 
-	// -- ----------------------------------
-	// You can be implicit:
+	fmt.Println()
 
+	// -- ---------------------------
+	// Implicit
 	implementation2 := func(a string, b string) string {
-		return fmt.Sprintf("%s to the %s", a, b)
+		return fmt.Sprintf("%s %s", b, a)
 	}
+
+	fmt.Println("Implementation 2:")
 
 	wazzup(implementation2)
 
-	// -- ----------------------------------
-	// You can even be anonymous:
+	fmt.Println()
+
+	// -- ---------------------------
+	// More implicit
+	fmt.Println("Implementation 3:")
+
+	wazzup(implementation3)
+
+	fmt.Println()
+
+	// -- ---------------------------
+	// Anonymous
+	fmt.Println("Anonymous:")
 
 	wazzup(
 		func(a string, b string) string {
-			return fmt.Sprintf("%s, I want to say %s", b, a)
+			return fmt.Sprintf("%s %s!!!!", a, b)
 		},
 	)
+
 }
