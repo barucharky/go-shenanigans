9ca4b4f799d1fb15ab91aaa70a9020dc771cf326
barucharky <benarky@gmail.com>
2020-06-15 22:20:36 +0300

    playing with regexp package

--- playground/z-sandbox-garbage/garbage.go
+++ playground/z-sandbox-garbage/garbage.go
@@ -4,25 +4,50 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"regexp"
+	"strings"
 )
 
 func main() {
 
+	// get search
+	var search string = os.Args[1]
 	directory, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fileStat, err := os.Lstat(directory)
+	// Get the list of files in the search directory
+	files := fetchFiles(directory)
+	search = strings.ToLower(search)
+
+	for _, file := range files {
+
+		var filename string = strings.ToLower(file.Name())
+
+		match, err := regexp.MatchString(search, filename)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if match {
+			fmt.Println("Match:\n", filename)
+		}
+	}
+
+}
+
+func fetchFiles(directory string) []os.FileInfo {
+	var files []os.FileInfo
+	var err error
+
+	files, err = ioutil.ReadDir(directory)
 	if err != nil {
-		fmt.Println("dirTest error")
 		log.Fatal(err)
 	}
 
-	fileMode := fileStat.Mode()
-
-	fmt.Println(fileMode.IsDir())
-
+	return files
 }
